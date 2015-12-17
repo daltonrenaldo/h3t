02634c4c44d71c8e9835f4b66cd7830a1d35d746
Renaldo Pierre-Louis <pirelouisd87@gmail.com>
2015-12-16 23:15:44 -0500

    switched to jquery-ui draggable for mobile with cordova

--- client/lib/h3t.js
+++ client/lib/h3t.js
@@ -1,12 +1,15 @@
 class H3T {
   constructor() {
     this.board = new Board();
+    this.numberOfMoves = 0;
+    this.turnTracker = new Tracker.Dependency;
   }
 
   handleClick (cell) {
     var cellValue = this.board.getCell(cell.row, cell.col).value;
-    if (_.isEmpty(cellValue)) {
-      this.board.setCell(cell.row, cell.col, "X");
+    if (_.isEmpty(cellValue) && this.numberOfMoves < this.board.size * 2) {
+      this.board.setCell(cell.row, cell.col, this.playerTurn[1]);
+      this.incrementMovesCount();
     } else {
       console.log("moveError", "Cell isn't Empty")
     }
@@ -22,6 +25,7 @@ class H3T {
       var pieceData = this.board.getCell(pieceRow, pieceCol);
       this.board.setCell(cellRow, cellCol, pieceData.value);
       this.board.clearCell(pieceRow, pieceCol);
+      this.incrementMovesCount();
     } else {
       console.log("moveError", "Can't move piece ("+pieceRow+ "," + pieceCol + ") to cell ("+cellRow + ","+ cellCol +")");
     }
@@ -38,6 +42,16 @@ class H3T {
     var cSq = Math.pow(a.data("row") - b.data("row"), 2) +
                 Math.pow(a.data("col") - b.data("col"), 2);
     return Math.sqrt(cSq);
+  }
+
+  incrementMovesCount () {
+    this.numberOfMoves = this.numberOfMoves + 1;
+    this.turnTracker.changed();
+  }
+
+  get playerTurn () {
+    this.turnTracker.depend();
+    return this.numberOfMoves % 2 == 0 ? ['player-1', 'X'] : ['player-2', 'O'];
   }
 }
 
--- tictactoe.js
+++ tictactoe.js
@@ -2,6 +2,12 @@ if (Meteor.isClient) {
   Template.Board.helpers({
     rows: function () {
       return h3t.board.grid;
+    },
+    playerturn: function () {
+      return h3t.playerTurn[0];
+    },
+    deviceType: function () {
+      return Meteor.isCordova ? 'mobile' : 'desktop';
     }
   });
 
@@ -14,20 +20,43 @@ if (Meteor.isClient) {
     },
 
     "dragstart .pawn": function (event, template) {
-      event.originalEvent.dataTransfer.setData("text", event.target.id);
+      console.log("dragstart");
+      console.log(event.originalEvent);
+      console.log($(event.originalEvent.target).addClass('moving'))
+      // event.originalEvent.dataTransfer.setData("text", event.target.id);
     },
 
-    "dragover td": function (event, template) {
+    "dragstop .pawn": function (event, template) {
+      console.log("dragstop");
+      $(event.originalEvent.target).removeClass('moving');
+    },
+
+    "dropover td": function (event, template) {
+      console.log("dropover");
       event.originalEvent.preventDefault();
     },
 
     "drop td": function (event, template) {
-      var data = event.originalEvent.dataTransfer.getData("text");
-      var $piece = $("#"+data);
+      console.log("drop");
+      var $piece = $('.pawn.moving');
       var $cell = $(event.target);
+
+      console.log($piece, $cell);
 
       h3t.move($piece, $cell);
     }
+  });
+
+
+  Template.Board.onRendered(function () {
+    $('td').droppable();
+  });
+
+  Template.pawn.onRendered(function () {
+    $('.pawn').draggable({ revert: true });
+  });
+
+  Template.Board.onDestroyed(function () {
   });
 }
 
